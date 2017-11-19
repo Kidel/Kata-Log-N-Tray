eedb3df0e53a1544d851f5c2cd6996e303708e38
Gaetano Bonofiglio <gaetano.bonofiglio@gmail.com>
2017-11-19 23:35:56 +0100

    removed a useless static

--- LogNTray/src/com/logntray/bonofiglio/LogNTray.java
+++ LogNTray/src/com/logntray/bonofiglio/LogNTray.java
@@ -1,9 +1,9 @@
 package com.logntray.bonofiglio;
 
 public class LogNTray {
-	@SuppressWarnings("unused")
-	private static UI ui = new UI();
-	
-	public static void main(String[] args) { }
+
+	public static void main(String[] args) {
+		new UI();
+	}
 
 }
--- LogNTray/src/com/logntray/bonofiglio/UI.java
+++ LogNTray/src/com/logntray/bonofiglio/UI.java
@@ -1,6 +1,5 @@
 package com.logntray.bonofiglio;
 
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -9,13 +8,13 @@ import java.net.URL;
 
 public class UI {
 
-	private static LoginFrame frame;
-	private static TrayIcon trayIcon;
-	private static SystemTray tray;
+	private LoginFrame frame;
+	private TrayIcon trayIcon;
+	private SystemTray tray;
 
-	private static String title;
+	private String title;
 
-	private static Repository r = new Repository();
+	private Repository r = new Repository();
 
 	UI() {
 		title = "Log 'n Tray";
@@ -56,12 +55,12 @@ public class UI {
 		return ret;
 	}
 
-	private static void showInfoTray(String message) {
+	private void showInfoTray(String message) {
 		trayIcon.displayMessage(title,
 				message, TrayIcon.MessageType.INFO);
 	}
 
-	private static void createAndShowGUI() {
+	private void createAndShowGUI() {
 		frame = new LoginFrame(title);
 		frame.setActionLogin(
 				new ActionListener(){
@@ -73,6 +72,7 @@ public class UI {
 									JOptionPane.INFORMATION_MESSAGE);
 							frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 							frame.dispose();
+							setTrayIcon("online","png");
 						} else {
 							JOptionPane.showMessageDialog(null,
 									"Invalid username or password",
@@ -103,7 +103,7 @@ public class UI {
 			return;
 		}
 		final PopupMenu popup = new PopupMenu();
-		trayIcon = new TrayIcon(createImage("../../../images/online.png", "online"));
+		trayIcon = new TrayIcon(createImage("../../../images/offline.png", "offline"));
 		tray = SystemTray.getSystemTray();
 
 		trayIcon.setToolTip(title);
@@ -172,11 +172,11 @@ public class UI {
 		});
 	}
 
-	protected static URL getFileURL(String path) {
+	protected URL getFileURL(String path) {
 		return LogNTray.class.getResource(path);
 	}
 
-	protected static Image createImage(String path, String description) {
+	protected Image createImage(String path, String description) {
 		URL imageURL = getFileURL(path);
 		if (imageURL == null) {
 			System.err.println("Resource not found: " + path);
@@ -186,7 +186,7 @@ public class UI {
 		}
 	}
 	
-	protected static void setTrayIcon(String iconName, String iconExt) {
+	protected void setTrayIcon(String iconName, String iconExt) {
 		trayIcon.setImage(createImage("../../../images/"+iconName+"."+iconExt, iconName));
 	}
 
